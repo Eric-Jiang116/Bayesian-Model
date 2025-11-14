ff9b05bb53d43f69ace50958e311989c17c6e213
Isabella Pei <izzypei@berkeley.edu>
2025-11-14 11:02:27 -0800

    adjusting rate split

--- vcDNN.py
+++ vcDNN.py
@@ -5,46 +5,50 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
-VCData = pd.read_csv("vc_training_and_test_set.csv")
-v  = pd.read_csv("v_pred.csv")   # v_pred
-vc = pd.read_csv("beta_pred.csv")          # (N, P), actual VC function values
-X_sub = pd.read_csv("X_sub.csv")
-rate = pd.read_csv("r_pred.csv")
+v  = pd.read_csv("v_pred.csv")      # v_pred
+vc = pd.read_csv("beta_pred.csv")   # (N, P), actual VC function values
+X_sub = pd.read_csv("X_sub.csv")    # (n_subjects, P)
+rate = pd.read_csv("r_pred.csv")    # (N, n_subjects)
 P  = vc.shape[1]
 
 # split 60/20/20
-v_train, v_test, vc_train, vc_test = train_test_split(v, vc, test_size=0.2, random_state=42)
-v_train, v_val, vc_train, vc_val = train_test_split(v_train, vc_train, test_size=0.25, random_state=42)
+v_train, v_test, vc_train, vc_test, rate_train, rate_test = train_test_split(v, vc, rate, test_size=0.2, random_state=42)
+v_train, v_val, vc_train, vc_val, rate_train, rate_val = train_test_split(v_train, vc_train, rate_train, test_size=0.25, random_state=42)
 
 # scale with TRAIN ONLY
 v_mean, v_std = v_train.mean(), v_train.std()
 norm = lambda x: (x - v_mean) / (v_std + 1e-8)
 
-Xtrain, Xval, Xtest = map(norm, (v_train, v_val, v_test))
-
 # model
 def VCNet(P, hidden=(64,64), dropout=0.2):
-    layers, in_dim = [], 1
+    layers, in_dim = [], 1 # input is 1D: time variable
     for h in hidden:
         layers += [nn.Linear(in_dim, h), nn.ReLU(), nn.Dropout(dropout)]
         in_dim = h
     layers += [nn.Linear(in_dim, P)]
     return nn.Sequential(*layers)
 
+# rate function for training
 def rate_fn(X_sub, vc):
-    return torch.exp((X_sub * vc).sum(dim=1, keepdim=True))
+    """
+    beta: [N, P]
+    X_sub: [n_subjects, P]
+    returns rate: [N, n_subjects]
+    """
+    return torch.exp(X_sub @ vc.T).T # transpose 
 
+Xtrain, Xval, Xtest = map(norm, (v_train, v_val, v_test))
 # convert to tensor
 X_sub = torch.tensor(X_sub).float()
 rate = torch.tensor(rate).float()
 
-Xtrain = torch.tensor(Xtrain)
-Xval = torch.tensor(Xval)
-Xtest = torch.tensor(Xtest)
+Xtrain = torch.tensor(Xtrain).float()
+Xval = torch.tensor(Xval).float()
+Xtest = torch.tensor(Xtest).float()
 
-Ytrain =   # rate train = exp(xsub * vc train)
-Yval = 
-Ytest = rate
+Ytrain = torch.tensor(rate_train).float()
+Yval = torch.tensor(rate_val).float()
+Ytest = torch.tensor(rate_test).float()
 
 model = VCNet(P)
 opt = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-4)
@@ -54,7 +58,7 @@ loss_fn = nn.MSELoss()
 for epoch in range(2000):
     model.train()
     opt.zero_grad()
-    rate_pred = rate_fn(model(Xtrain), X_sub) # get rate prediction
+    rate_pred = rate_fn(model(Xtrain), X_sub) # predict vc function + convert to rate
     loss = loss_fn(rate_pred, Ytrain) # loss on rate
     loss.backward()
     opt.step()
@@ -70,20 +74,22 @@ vmin, vmax = v_test.min(), v_test.max() # plot only test range for interpolation
 v_grid = np.linspace(vmin, vmax, 200).astype(np.float32).reshape(-1,1)
 model.eval()
 with torch.no_grad():
-    vc_pred_grid = model(torch.from_numpy(norm(v_grid))).cpu().numpy()
+    vc_pred = model(torch.from_numpy(norm(v_grid)))
+    rate_pred = rate_fn(X_sub, vc_pred).cpu().numpy()
+
 # Plot: use the full truth (sorted by v) and the smooth predicted line
-order = np.argsort(v_test[:,0]) # indices to sort test set, so that v and vc have the same index order for plotting
+order = np.argsort(v_test[:,0]) # indices to sort test set, so that v and rate have the same index order for plotting
 v_sorted  = v_test[order, 0]
-vc_sorted = vc_test[order]
+rate_sorted = rate_test[order]
 
 plt.figure(figsize=(8,5))
 for j in range(P):
     if j == 0:
-        plt.scatter(v_sorted, vc_sorted[:, j], s=25, alpha=0.6, label="True (test)")
-        plt.plot(v_grid[:, 0], vc_pred_grid[:, j], label="Predicted")
+        plt.scatter(v_sorted, rate_sorted[:, j], s=25, alpha=0.6, label="True (test)")
+        plt.plot(v_grid[:, 0], rate_pred[:, j], label="Predicted")
     else:
-        plt.scatter(v_sorted, vc_sorted[:, j], s=25, alpha=0.6)
-        plt.plot(v_grid[:, 0], vc_pred_grid[:, j])
-plt.xlabel("v"); plt.ylabel("VC function values")
+        plt.scatter(v_sorted, rate_sorted[:, j], s=25, alpha=0.6)
+        plt.plot(v_grid[:, 0], rate_pred[:, j])
+plt.xlabel("v"); plt.ylabel("rate values")
 plt.legend()
 plt.show()
