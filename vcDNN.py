8cd0c8670e20e294611532437a29e11ad165e3c0
Isabella Pei <izzypei@berkeley.edu>
2025-11-21 18:58:20 -0800

    forgot

--- vcDNN.py
+++ vcDNN.py
@@ -5,10 +5,10 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
-v  = pd.read_csv("v_pred.csv")      # v_pred
-vc = pd.read_csv("beta_pred.csv")   # (N, P), actual VC function values
-X_sub = pd.read_csv("X_sub.csv")    # (n_subjects, P)
-rate = pd.read_csv("r_pred.csv")    # (N, n_subjects)
+v  = pd.read_csv("v_pred.csv").to_numpy(np.float32)     # v_pred
+vc = pd.read_csv("beta_pred.csv").to_numpy(np.float32)  # (N, P), actual VC function values
+X_sub_full = pd.read_csv("X_sub.csv").to_numpy(np.float32)   # (n_subjects, P)
+rate = pd.read_csv("r_pred.csv").to_numpy(np.float32)    # (N, n_subjects)
 P  = vc.shape[1]
 
 # split 60/20/20
@@ -44,8 +44,8 @@ def rate_fn(X_sub, vc):
 
 Xtrain, Xval, Xtest = map(norm, (v_train, v_val, v_test))
 # convert to tensor
-X_sub = torch.tensor(X_sub.to_numpy()).float()
-rate = torch.tensor(rate.to_numpy()).float()
+X_sub_full = torch.tensor(X_sub_full).float()
+rate = torch.tensor(rate).float()
 
 Xtrain = torch.tensor(Xtrain).float()
 Xval = torch.tensor(Xval).float()
@@ -55,6 +55,7 @@ Ytrain = torch.tensor(rate_train).float()
 Yval = torch.tensor(rate_val).float()
 Ytest = torch.tensor(rate_test).float()
 
+X_sub = X_sub_full[:Ytrain.shape[1]]
 model = VCNet(P)
 opt = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-4)
 loss_fn = nn.MSELoss()
